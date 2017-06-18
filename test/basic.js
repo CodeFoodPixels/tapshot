3a5074120ffdc53d4337a7160929b8ff76d9afa7
Luke Bonaccorsi <luke@lukeb.co.uk>
2017-06-18 21:09:54 +0100

    Add tests for basic and option-modified cases

---
+++ test/basic.js
@@ -0,0 +1,124 @@
+'use strict';
+
+const tap = require(`tap`);
+const proxyquire = require(`proxyquire`);
+const fs = require(`fs`);
+const sinon = require(`sinon`);
+
+const fixture = fs.readFileSync(`${__dirname}/fixtures/basic.json`, `utf8`);
+
+const tapshot = proxyquire(`../index.js`, {
+    fs: {
+        readFileSync(fileName) {
+            tap.equal(fileName, `snapshots/basic.js.snap`);
+            return fixture;
+        }
+    }
+});
+
+tap.test(`run with defaults`, (t) => {
+    const tMock = {
+        name: `pass`,
+        equal: sinon.spy(),
+        pass: sinon.spy()
+    };
+
+    tapshot(tMock, {mockData: "this is fake data"});
+
+    t.ok(tMock.equal.calledOnce);
+    t.ok(tMock.pass.notCalled);
+
+    t.end();
+});
+
+tap.test(`run with defaults and tap as a function`, (t) => {
+    const tMock = new (function() {
+        this.name = `pass`;
+        this.equal = sinon.spy();
+        this.pass = sinon.spy();
+    })();
+
+    tapshot(tMock, {mockData: "this is fake data"});
+
+    t.ok(tMock.equal.calledOnce);
+    t.ok(tMock.pass.notCalled);
+
+    t.end();
+});
+
+tap.test(`throws when tap isn't an object or function`, (t) => {
+    t.throws(
+        () => {
+            tapshot('badger', {mockData: "this is fake data"})
+        },
+        `tap must either be an object or a function`
+    );
+    t.end();
+});
+
+tap.test(`throws when tap.equal isn't a function`, (t) => {
+    const tMock = {
+        name: `fail`,
+        equal: `badger`,
+        pass: sinon.spy()
+    };
+    t.throws(
+        () => {
+            tapshot(tMock, {mockData: "this is fake data"})
+        },
+        `tap must have the 'equal' assertion method`
+    );
+    t.ok(tMock.pass.notCalled);
+    t.end();
+});
+
+tap.test(`throws when tap.pass isn't a function`, (t) => {
+    const tMock = {
+        name: `fail`,
+        equal: sinon.spy(),
+        pass: `badger`
+    };
+    t.throws(
+        () => {
+            tapshot(tMock, {mockData: "this is fake data"})
+        },
+        `tap must have the 'pass' assertion method`
+    );
+    t.ok(tMock.equal.notCalled);
+    t.end();
+});
+
+tap.test(`throws when tap.name isn't a string`, (t) => {
+    const tMock = {
+        name: [`fail`],
+        equal: sinon.spy(),
+        pass: sinon.spy()
+    };
+    t.throws(
+        () => {
+            tapshot(tMock, {mockData: "this is fake data"})
+        },
+        `No name provided, either use this within a named test or set options.name`
+    );
+    t.ok(tMock.equal.notCalled);
+    t.ok(tMock.pass.notCalled);
+    t.end();
+});
+
+tap.test(`throws when tap.name is an empty string`, (t) => {
+    const tMock = {
+        name: ``,
+        equal: sinon.spy(),
+        pass: sinon.spy()
+    };
+    t.throws(
+        () => {
+            tapshot(tMock, {mockData: "this is fake data"})
+        },
+        `No name provided, either use this within a named test or set options.name`
+    );
+    t.ok(tMock.equal.notCalled);
+    t.ok(tMock.pass.notCalled);
+    t.end();
+});
+
---
+++ test/options.js
@@ -0,0 +1,224 @@
+'use strict';
+
+const tap = require(`tap`);
+const proxyquire = require(`proxyquire`);
+const fs = require(`fs`);
+const sinon = require(`sinon`);
+
+const fixture = fs.readFileSync(`${__dirname}/fixtures/basic.json`, `utf8`);
+
+const fsOverrides = {
+    readFileSync(fileName) {
+        tap.equal(fileName, `snapshots/options.js.snap`);
+        return fixture;
+    }
+}
+
+const tapshot = proxyquire(`../index.js`, {
+    fs: fsOverrides
+});
+
+
+tap.test(`throws when options isn't an object`, (t) => {
+    const tMock = {
+        name: `fail`,
+        equal: sinon.spy(),
+        pass: sinon.spy()
+    };
+    t.throws(
+        () => {
+            tapshot(tMock, {mockData: "this is fake data"}, `badger`)
+        },
+        `options must be an object`
+    );
+    t.ok(tMock.equal.notCalled);
+    t.ok(tMock.pass.notCalled);
+    t.end();
+});
+
+tap.test(`throws when options is an array`, (t) => {
+    const tMock = {
+        name: `fail`,
+        equal: sinon.spy(),
+        pass: sinon.spy()
+    };
+    t.throws(
+        () => {
+            tapshot(tMock, {mockData: "this is fake data"}, [])
+        },
+        `options must be an object`
+    );
+    t.ok(tMock.equal.notCalled);
+    t.ok(tMock.pass.notCalled);
+    t.end();
+});
+
+tap.test(`runs when tap.name isn't defined but is defined in options`, (t) => {
+    const tMock = {
+        equal: sinon.spy(),
+        pass: sinon.spy()
+    };
+
+    tapshot(tMock, {mockData: "this is fake data"}, {name:`pass`})
+
+    t.ok(tMock.equal.calledOnce);
+    t.ok(tMock.pass.notCalled);
+    t.end();
+});
+
+tap.test(`throws when tap.name is undefined and options.name is an empty string`, (t) => {
+    const tMock = {
+        equal: sinon.spy(),
+        pass: sinon.spy()
+    };
+    t.throws(
+        () => {
+            tapshot(tMock, {mockData: "this is fake data"}, {name:``})
+        },
+        `No name provided, either use this within a named test or set options.name`
+    );
+    t.ok(tMock.equal.notCalled);
+    t.ok(tMock.pass.notCalled);
+    t.end();
+});
+
+tap.test(`runs when file is overridden in options`, (t) => {
+    const tMock = {
+        name: `pass`,
+        equal: sinon.spy(),
+        pass: sinon.spy()
+    };
+
+    fsOverrides.readFileSyncBk = fsOverrides.readFileSync;
+    fsOverrides.readFileSync = (fileName) => {
+        t.equal(fileName, `snapshots/badger.snap`);
+        return fixture;
+    };
+
+    tapshot(tMock, {mockData: "this is fake data"}, {file:`snapshots/badger.snap`})
+
+    t.ok(tMock.equal.calledOnce);
+    t.ok(tMock.pass.notCalled);
+
+    fsOverrides.readFileSync = fsOverrides.readFileSyncBk;
+    fsOverrides.readFileSyncBk = undefined;
+
+    t.end();
+});
+
+tap.test(`runs when serializer is passed as a function in options`, (t) => {
+    const tMock = {
+        name: `pass`,
+        equal: sinon.spy(),
+        pass: sinon.spy()
+    };
+
+    const serializerStub = sinon.stub().callsFake((object) => {
+        return JSON.stringify(object);
+    });
+
+    tapshot(tMock, {mockData: "this is fake data"}, {serializer: serializerStub});
+
+    t.ok(serializerStub.calledOnce);
+    t.ok(tMock.equal.calledOnce);
+    t.ok(tMock.pass.notCalled);
+
+    t.end();
+});
+
+
+tap.test(`runs when serializer is passed as a string in options`, (t) => {
+    const tMock = {
+        name: `pass`,
+        equal: sinon.spy(),
+        pass: sinon.spy()
+    };
+
+    const obj = {
+        serialize: sinon.stub().callsFake(() => {
+            return JSON.stringify({mockData: "this is fake data"});
+        })
+    }
+
+    tapshot(tMock, obj, {serializer: 'serialize'});
+
+    t.ok(obj.serialize.calledOnce);
+    t.ok(tMock.equal.calledOnce);
+    t.ok(tMock.pass.notCalled);
+
+    t.end();
+});
+
+tap.test(`throws when serializer is not passed as a string or function in options`, (t) => {
+    const tMock = {
+        name: `fail`,
+        equal: sinon.spy(),
+        pass: sinon.spy()
+    };
+    t.throws(
+        () => {
+            tapshot(tMock, {mockData: "this is fake data"}, {serializer: [`fail`]})
+        },
+        `Serializer provided to options.serializer must be a funtion or a string with the name of the serializer to be called on the object`
+    );
+
+    t.ok(tMock.equal.notCalled);
+    t.ok(tMock.pass.notCalled);
+    t.end();
+});
+
+tap.test(`throws when serializer specified as string does not exist on the object`, (t) => {
+    const tMock = {
+        name: `fail`,
+        equal: sinon.spy(),
+        pass: sinon.spy()
+    };
+    t.throws(
+        () => {
+            tapshot(tMock, {mockData: "this is fake data"}, {serializer: `badger`})
+        },
+        `Method 'badger' does not exist on provided object`
+    );
+
+    t.ok(tMock.equal.notCalled);
+    t.ok(tMock.pass.notCalled);
+    t.end();
+});
+
+tap.test(`throws when serializer specified as string is on the object but is not a function`, (t) => {
+    const tMock = {
+        name: `fail`,
+        equal: sinon.spy(),
+        pass: sinon.spy()
+    };
+    t.throws(
+        () => {
+            tapshot(tMock, {mockData: "this is fake data", badger: true}, {serializer: `badger`})
+        },
+        `Method 'badger' does not exist on provided object`
+    );
+
+    t.ok(tMock.equal.notCalled);
+    t.ok(tMock.pass.notCalled);
+    t.end();
+});
+
+tap.test(`throws when serializer does not produce a string`, (t) => {
+    const tMock = {
+        name: `fail`,
+        equal: sinon.spy(),
+        pass: sinon.spy()
+    };
+    t.throws(
+        () => {
+            tapshot(tMock, {mockData: "this is fake data"}, {serializer: () => {
+                return {badger: true};
+            }})
+        },
+        `Serializer provided to options.serializer must produce a string`
+    );
+
+    t.ok(tMock.equal.notCalled);
+    t.ok(tMock.pass.notCalled);
+    t.end();
+});
